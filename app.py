92f59efc19b9192defe61e53622aa8fa6bafb0c2
nousset <hermann.glan@amani.consulting>
2025-04-24 15:39:12 +0000

    commite-render001

--- app.py
+++ app.py
@@ -690,4 +690,7 @@ if __name__ == "__main__":
     </svg>''')
     
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=False)
+    app.run(host="0.0.0.0", port=port)
+
+
+   
